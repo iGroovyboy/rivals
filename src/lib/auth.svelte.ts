5609638ee36637043b660efd93558ffaecaf2251
babintsev <tbabintsev2@gmail.com>
2024-12-27 19:30:53 +0200

    fix build

--- src/lib/auth.svelte.ts
+++ src/lib/auth.svelte.ts
@@ -1,8 +1,10 @@
-import { PUBLIC_API_KEY, PUBLIC_API_URL } from '$env/static/public';
 import { createClient } from '@supabase/supabase-js';
 import { toast, TOAST_COLOR } from '$lib/toasts.svelte';
 
-export const supabase = createClient(PUBLIC_API_URL, PUBLIC_API_KEY);
+export const supabase = createClient(
+	'https://agqsdtupansgyvtqfsgl.supabase.co',
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFncXNkdHVwYW5zZ3l2dHFmc2dsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQzOTA5ODMsImV4cCI6MjA0OTk2Njk4M30.sD8kYscPLAmJeZB-BMu_wALFhYRtkaTL1CorhTFUD34'
+);
 
 export const supabaseLogin = async () => {
 	const { data, error } = await supabase.auth.signInWithOAuth({
